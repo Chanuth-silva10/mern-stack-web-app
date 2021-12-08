dbdc3a7682697bc2353b7e165f0c8b1463ef7fca
Chanuth10 <it20074418@my.sliit.lk>
2021-12-08 12:32:26 +0530

    Create db.js file and import this file into the server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const dbConnection = require("./db"); // calling db.js
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-
- 
-
-
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
