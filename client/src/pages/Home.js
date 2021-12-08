76c2f46ea697f78ca02918bed3c9ce0ae46fe1b6
Chanuth10 <it20074418@my.sliit.lk>
2021-12-09 02:43:16 +0530

    Implement redux folder struture

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import DefaultLayout from '../components/DefaultLayout'
+import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
+import { useSelector, useDispatch } from "react-redux";
 
 function Home() {
-    return (
-        <DefaultLayout>
-           <h1>Home</h1> 
-        </DefaultLayout>
-         
-        
-    )
+  const { cars } = useSelector((state) => state.carsReducer);
+  return (
+    <DefaultLayout>
+      <h1>Home</h1>
+      <h1>{cars.length}</h1>
+    </DefaultLayout>
+  );
 }
 
-export default Home
+export default Home;
