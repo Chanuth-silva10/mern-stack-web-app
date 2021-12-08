4ce11eaa468978835073c292ace412af14ad4a0b
Chanuth10 <it20074418@my.sliit.lk>
2021-12-08 10:20:08 +0530

    Crete DefaultLayout for Home.js

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {Route ,Routes, BrowserRouter , Redirect} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import BookingCar from './pages/BookingCar'
+
+function App() {
+  return (
+    <div className="App">
+     
+    <BrowserRouter>
+        <Routes>
+             <Route path='/' element={<Home/>} />
+             <Route path='/login' element={<Login/>} />
+             <Route path='/register' element={<Register/>} />
+             <Route path='/bookingcar' element={<BookingCar/>} />
+        </Routes>
+
+    </BrowserRouter>
+
+    </div>
+  );
+}
+ 
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+
+function Home() {
+    return (
+        <DefaultLayout>
+           <h1>Home</h1> 
+        </DefaultLayout>
+         
+        
+    )
+}
+
+export default Home
