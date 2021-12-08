471405a0f30e60fc6b4bc54643d872239375cf84
Chanuth10 <it20074418@my.sliit.lk>
2021-12-09 03:25:11 +0530

    Check Car details from Backend API call

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useSelector, useDispatch } from "react-redux";
+import { getAllCars } from "../redux/actions/carsActions";
 
 function Home() {
   const { cars } = useSelector((state) => state.carsReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCars());
+  }, []);
+
   return (
     <DefaultLayout>
       <h1>Home</h1>
