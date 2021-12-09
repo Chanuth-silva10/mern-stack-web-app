ed9e7cce8c05893ca8cda9fe19d950661510a470
Chanuth10 <it20074418@my.sliit.lk>
2021-12-10 00:56:08 +0530

    Generate Logout page

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { Menu, Dropdown, Button, Space, Row, Col } from "antd";
+import { Link } from "react-router-dom";
 
 function DefaultLayout(props) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a href="/">Home</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/userbookings">Bookings</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/admin">Admin</a>
+      </Menu.Item>
+      <Menu.Item
+        onClick={() => {
+          localStorage.removeItem("user");
+          window.location.href = "/login";
+        }}
+      >
+        <li style={{ color: "orangered" }}>Logout</li>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <div>
       <div className="header bs1">
         <div className="d-flex justify-content-between">
           <h1>SheyCars</h1>
+
+          <Dropdown overlay={menu} placement="bottomCenter">
+            <Button>Hi {user.username}</Button>
+          </Dropdown>
         </div>
       </div>
       <div className="content">{props.children}</div>
